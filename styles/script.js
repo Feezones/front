2cb1c9041162a2f2b778923523785f2703530c23
Felipe Braghittoni <felipe.braghittoni1999@gmail.com>
2022-06-17 00:54:40 -0300

    correção de diretorios

---
+++ styles/script.js
@@ -0,0 +1,11 @@
+function redLogin(){
+        window.location.href = "/templates/login.html"
+    }
+
+function redCadastro(){
+    window.location.href = "/templates/Cadastro.html"
+}
+
+function redIndex(){
+    window.location.href = "/templates/index.html"
+}
